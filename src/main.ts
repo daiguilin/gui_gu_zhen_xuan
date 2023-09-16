1793143ecf7f91d183b0d75a2f84b874fc102f4b
daiguilin <10072882+daigguilin@user.noreply.gitee.com>
2023-09-16 18:43:40 +0800

    format

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
 
 const fun = () => {
-  console.log("没有格式化代码");
-};
-fun();
+  console.log('没有格式化代码')
+}
+fun()
